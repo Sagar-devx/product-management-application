0c61fa284fcc515e608dafc84a844c9ca8ed3bc2
Sagar Sharma <sagarsharma.devx@gmail.com>
2026-07-09 10:41:41 +0530

    Clean up Product_Dao.java by removing blank lines
    Removed unnecessary blank lines for cleaner code.

--- src/main/java/product/Dao/Product_Dao.java
+++ src/main/java/product/Dao/Product_Dao.java
@@ -1,13 +1,10 @@
 package product.Dao;
 
 import java.util.List;
-
 import javax.transaction.Transactional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.orm.hibernate5.HibernateTemplate;
 import org.springframework.stereotype.Component;
-
-
 import product.Model.Product;
 
 
@@ -18,22 +15,17 @@ public class Product_Dao
 	private HibernateTemplate hibernateTemplate;
 	
 	//Create Product
-	
 	@Transactional
 	public void createProduct(Product product)
 	{
 		this.hibernateTemplate.saveOrUpdate(product);
-		
 	}
 	
 	//Get all Products
-	
 	public List<Product> getAllProducts()
 	{
 	  List<Product> products = this.hibernateTemplate.loadAll(Product.class);
-		
 		return products;
-		
 	}
 	
 	// Delete single Product
@@ -45,16 +37,9 @@ public class Product_Dao
 	}
 	
 	// Get Single Product
-	
 	public Product getProduct(int product_id)
 	{
 		 Product product = this.hibernateTemplate.get(Product.class,product_id);
 		 return product;
 	}
-	
-	
-	
-	
-	
-
 }
