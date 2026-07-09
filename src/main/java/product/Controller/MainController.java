70a489985bf89c07b545256aac1fc6feb7026f83
Sagar Sharma <sagarsharma.devx@gmail.com>
2026-07-09 10:40:44 +0530

    Refactor MainController by removing blank lines
    Removed unnecessary blank lines and improved formatting in MainController.java.

--- src/main/java/product/Controller/MainController.java
+++ src/main/java/product/Controller/MainController.java
@@ -1,10 +1,7 @@
 package product.Controller;
 
-
 import java.util.List;
-
 import javax.servlet.http.HttpServletRequest;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -13,7 +10,6 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.view.RedirectView;
-
 import product.Dao.Product_Dao;
 import product.Model.Product;
 
@@ -32,54 +28,38 @@ public class MainController
 	}
 	
 	// For add Product
-	
 	@RequestMapping("/add-product")
 	public String addProduct(Model m)
 	{
 		m.addAttribute("title", "Add Product");
-		return "add_product_form";
-				
+    	return "add_product_form";			
 	}
-	
+
 	@RequestMapping(value="/handle",method=RequestMethod.POST)
 	public RedirectView handleProduct(@ModelAttribute Product product,HttpServletRequest request)
 	{
-		
 		productdao.createProduct(product);
 		RedirectView redirectView= new RedirectView();
-		
 		redirectView.setUrl(request.getContextPath()+"/");
 		return redirectView;
-		
 	}
 	
 	// For Delete Product
-	
 	@RequestMapping("/delete/{product-id}")
 	public RedirectView deleteProduct(@PathVariable("product-id") int id ,HttpServletRequest request)
 	{
 	   this.productdao.deleteProduct(id);
 	   RedirectView redirectView= new RedirectView();
-		
-		redirectView.setUrl(request.getContextPath()+"/");
-		return redirectView;
-		
+	   redirectView.setUrl(request.getContextPath()+"/");
+	   return redirectView;
 	}
 	
 	// For Update Product
-	
 	@RequestMapping("/update/{id}")
 	public String updateform(@PathVariable("id") int id,Model m)
 	{
 		Product product = this.productdao.getProduct(id);
 		m.addAttribute("p", product);	
 		return "update_form";
-		
 	}
-	
-	
-
-	
-	
-
 }
